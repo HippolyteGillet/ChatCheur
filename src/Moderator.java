932eca714c251a7b0dd03207f29876389ad27250
gabriel <gabriel.henaux@edu.ece.fr>
2023-04-04 20:55:27 +0200

    User class ready 2

--- src/Moderator.java
+++ src/Moderator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Moderator {
+import java.time.LocalDate;
+
+public class Moderator extends User{
+
+    Moderator(int id, String userName, String password, String email, String firstName, String lastName, State state, LocalDate lastConnectionTime) {
+        super(id, userName, password, email, firstName, lastName, state, lastConnectionTime);
+        setStatus(Status.MODERATOR);
+        //DAO update:
+        //userDAO.update(this);
+    }
+
+    public void banUser(User user){
+        user.setPermission(Permission.BANNED);
+        //DAO update:
+        //userDAO.update(user);
+    }
+
+    public void unbanUser(User user){
+        user.setPermission(Permission.ACCEPTED);
+        //DAO update:
+        //userDAO.update(user);
+    }
+
+
 }
