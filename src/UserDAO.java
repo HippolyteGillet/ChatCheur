44726d9a6975611b6fdf2200c9d56a3eeb5c2a5e
nathan <nathan.outrey@edu.ece.fr>
2023-04-05 14:29:39 +0200

    fonction update

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -1,13 +1,14 @@
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 
 public class UserDAO extends DAO<User>{
 
-    public User create(User object){
+    /*public User create(User object){
         try {
-            ResultSet result = this.connect.createStatement(/*ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE*/).executeQuery("SELECT id FROM table");
+            ResultSet result = this.connect.createStatement(*//*ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE*//*).executeQuery("SELECT id FROM table");
             if (result.first()){
                 int id = result.getInt("id");
                 PreparedStatement preparedStatement = this.connect.prepareStatement("INSERT INTO table (id) VALUES ?");
@@ -21,27 +22,31 @@ public class UserDAO extends DAO<User>{
             e.printStackTrace();
         }
         return object;
-    }
+    }*/
 
-    public User find(int id){
+    /*public User find(int id){
         User user = new User();
         try {
             ResultSet result = this.connect.createStatement().executeQuery("");
+            if (result.first()){
+                user = new User(id, result.getString("userName"), result.getString("password"), result.getString("email"), result.getString("firstName"), result.getString("lastName"), result.getObject("state", ))
+            }
         }catch (SQLException e) {
             e.printStackTrace();
         }
         return user;
-    }
+    }*/
 
-    @Override
     public User update(User object) {
-        return null;
+        try {
+            this.connect.createStatement().executeUpdate("UPDATE table SET userName = '" + object.getUserName() + "', password = '" + object.getPassword() + "', email = '" + object.getEmail() + "', firstName = '" + object.getFirstName() + "', lastName = '" + object.getLastName() + "', permission = '" + object.getPermission() + "', status = '" + object.getStatus() + "', state = '" + object.getState() + "', lastConnectionTime = '" + object.getLastConnectionTime() + "' WHERE id = " + object.getId());
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return object;
     }
 
-    @Override
-    public void delete(User object) {
-
-    }
+    //public void delete(User object) {}
 
 
 
