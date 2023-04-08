b3da589e566285d35c452bb5fd9a46de3fc8dd33
nathan <nathan.outrey@edu.ece.fr>
2023-04-08 20:45:44 +0200

    fonction create ok

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         ConnectionDataBaseSQL.accessDriver();
         //UserDAO userDAO = new UserDAO();
-        //User user = new User();
+        //User user = new User(userDAO.newIdUser(), "userName", "password", "email", "firstName", "lastName", State.ONLINE, LocalDate.now());
+
+        // Test newIdUser() OK
+        //System.out.println(userDAO.newIdUser());
 
         // Test find() OK
         //System.out.println(userDAO.find(1));
 
+        // Test create() OK
+        //userDAO.create(user);
+
+        // Test delete()
+
+
+        // Test update()
 
     }
 }
