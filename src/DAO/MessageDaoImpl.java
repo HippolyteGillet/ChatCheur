2d109581ee1edc4ff50af1d175ea6a6f19fbb1b1
Stan <stanislas.bourseau@gmail.com>
2023-04-11 13:23:25 +0200

    Classe Message finie

--- src/DAO/MessageDaoImpl.java
+++ src/DAO/MessageDaoImpl.java
@@ -1,4 +1,25 @@
 package DAO;
 
+import modele.Message;
+
 public class MessageDaoImpl implements MessageDao{
+    @Override
+    public Message find(int id) {
+        return null;
+    }
+
+    @Override
+    public void create(Message object) {
+
+    }
+
+    @Override
+    public void update(Message obj) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
--- src/modele/Message.java
+++ src/modele/Message.java
@@ -1,4 +1,77 @@
 package modele;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+
 public class Message {
+    private int id;
+    private int user_id;
+    private LocalDateTime localDateTime;
+    private String content;
+
+    public Message() {
+        this.id = -1;
+        this.user_id = -1;
+        this.localDateTime = now();
+        this.content = "unknown";
+    }
+
+    public Message(int user_id, String content) {
+        this.user_id = user_id;
+        localDateTime = now();
+        this.content = content;
+    }
+
+    public Message(int id, int user_id, LocalDateTime localDateTime, String content) {
+        this.id = id;
+        this.user_id = user_id;
+        this.localDateTime = localDateTime;
+        this.content = content;
+    }
+
+    public Message(Message message) {
+        this.id = message.getId();
+        this.user_id = message.getUser_id();
+        this.localDateTime = message.getLocalDateTime();
+        this.content = message.getContent();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Id : " + this.id + "| User_id : " + this.user_id + " | TimeStamp : " + this.localDateTime + " | Content : " + this.content;
+    }
 }
