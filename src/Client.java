e7f5c9fb5264b92470314da8828b1710149f350c
Stan <stanislas.bourseau@gmail.com>
2023-04-18 11:25:10 +0200

    connexion et server non synchro

--- src/Client.java
+++ src/Client.java
@@ -47,9 +47,11 @@ class Client {
 
 
         //---------------------------------------SERVER PART----------------------------------------------
-        userModel.setUserName("Stan");
-        name = userModel.getUserName();
+        //userModel.setUserName("Stan");
+        //name = userModel.getUserName();
 
+        Scanner sc = new Scanner(System.in);
+        name = userModel.getUserName();
 
         try (Socket socket = new Socket("localhost", 9000)) {
 
@@ -58,6 +60,8 @@ class Client {
             // reading from server
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // sending the name to the server
+            String line = null;
 
             out.println(name + " has joined the chat");
             out.flush();
@@ -66,6 +70,29 @@ class Client {
             ThreadToDisplay threadToDisplay = new ThreadToDisplay(in);
 
             threadToDisplay.start();
+
+            while (!"exit".equalsIgnoreCase(line)) {
+
+                // reading from user
+                line = sc.nextLine();
+                switch (line) {
+                    case "exit":
+                        threadToDisplay.interrupt();
+                        System.out.println("You have left the chat");
+                        out.println(name + " has left the chat :(");
+                        out.flush();
+                        //TODO: Avertir avec view que le client a quitté le chat
+
+                        return;
+
+                    default:
+                        out.println(name + ": " + line);
+                        out.flush();
+                        break;
+                }
+
+
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
