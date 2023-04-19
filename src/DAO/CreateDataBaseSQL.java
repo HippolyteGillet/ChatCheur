89f694d01fc45e62d298898a02272c4fe8189f16
Stan <stanislas.bourseau@gmail.com>
2023-04-19 11:09:49 +0200

    fonction bannissement fix

--- src/DAO/CreateDataBaseSQL.java
+++ src/DAO/CreateDataBaseSQL.java
@@ -35,8 +35,7 @@ public class CreateDataBaseSQL {
                     "   TIMESTAMP timestamp    DEFAULT CURRENT_TIMESTAMP," +
                     "   TYPELOG   enum ('CONNECTION', 'DISCONNECTION', 'MESSAGE', 'BAN', 'UNBAN', 'SET_ROLE', 'UNKNOWN')," +
                     "   PRIMARY KEY (ID)," +
-                    "   FOREIGN KEY (USER_ID) REFERENCES chatcheur.user (ID))" +
-                    "ENGINE=InnoDB;");
+                    "   FOREIGN KEY (USER_ID) REFERENCES chatcheur.user (ID));");
             //On crée la table message
             statement.executeUpdate("CREATE TABLE chatcheur.message (" +
                     "   ID        int NOT NULL AUTO_INCREMENT," +
@@ -44,24 +43,23 @@ public class CreateDataBaseSQL {
                     "   TIMESTAMP timestamp    DEFAULT CURRENT_TIMESTAMP," +
                     "   CONTENT   varchar(255) DEFAULT NULL," +
                     "   PRIMARY KEY (ID)," +
-                    "   FOREIGN KEY (USER_ID) REFERENCES chatcheur.user (ID))" +
-                    "ENGINE=InnoDB;");
+                    "   FOREIGN KEY (USER_ID) REFERENCES chatcheur.user (ID));");
 
             //-----------------------DATA-MYSQL--------------------------------------------
             //On insert data dans user
             statement.executeUpdate("INSERT INTO chatcheur.user (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, " +
-                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Gaby', 'gabriel', 'henaux', 'gaby.henaux@gmail.com', '123', 'ADMINISTRATOR', CURRENT_TIMESTAMP, 'banned', 'OFFLINE')");
+                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Gaby', 'gabriel', 'henaux', 'gaby.henaux@gmail.com', 'e4feeef0e2f7ae25ab42a8ab7e3dfd1c6e3181b5321580b9d351952358dd1eb0', 'ADMINISTRATOR', CURRENT_TIMESTAMP, 'banned', 'OFFLINE')");
             statement.executeUpdate("INSERT INTO chatcheur.user (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, " +
-                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Stan', 'hippolyte', 'gillet', 'hippo.gillet@gmail.com', '123', 'ADMINISTRATOR', CURRENT_TIMESTAMP," +
+                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Stan', 'hippolyte', 'gillet', 'hippo.gillet@gmail.com', '0600a614e3541ebfbbdee287d5269a7615a3dd1cf17887bad36b4e6223fd1fa0', 'ADMINISTRATOR', CURRENT_TIMESTAMP," +
                     "   'ACCEPTED', 'OFFLINE')");
             statement.executeUpdate("INSERT INTO chatcheur.user (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, " +
-                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Chofo', 'nathan', 'outrey', 'nathoudu66@gmail.com', '123', 'ADMINISTRATOR', CURRENT_TIMESTAMP, 'ACCEPTED'," +
+                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('Chofo', 'nathan', 'outrey', 'nathoudu66@gmail.com', '0055e0a0380c5ea7a739d505decc453131c5b308f2a1936110b40a6cd828eb5b', 'ADMINISTRATOR', CURRENT_TIMESTAMP, 'ACCEPTED'," +
                     "   'OFFLINE')");
             statement.executeUpdate("INSERT INTO chatcheur.user (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, " +
-                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('jujulafondue', 'juliette', 'lafond', 'juliettelafond@gmail.com', '123', 'USER', CURRENT_TIMESTAMP," +
+                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('jujulafondue', 'juliette', 'lafond', 'juliettelafond@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', 'USER', CURRENT_TIMESTAMP," +
                     "   'ACCEPTED', 'OFFLINE')");
             statement.executeUpdate("INSERT INTO chatcheur.user (USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, " +
-                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('nul', 'nul', 'nul', 'nul@gmail.com', '123456789', 'MODERATOR', CURRENT_TIMESTAMP," +
+                    "PERMISSION, LAST_CONNECTION_T, ACCESS, STATE) VALUES ('nul', 'nul', 'nul', 'nul@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225', 'MODERATOR', CURRENT_TIMESTAMP," +
                     "   'ACCEPTED', 'OFFLINE')");
             //On insert data dans message
             statement.executeUpdate("INSERT INTO chatcheur.message (USER_ID, TIMESTAMP, CONTENT) VALUES (2, CURRENT_TIMESTAMP, 'Coucou comment vas-tu ?')");
