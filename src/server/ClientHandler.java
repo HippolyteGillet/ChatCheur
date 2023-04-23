d53bac5715f79af5ca386534b3fb86481b1cce2f
gabriel <gabriel.henaux@edu.ece.fr>
2023-04-23 23:51:18 +0200

    kick server ok

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -6,6 +6,7 @@ import model.user.User;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClientHandler extends Thread {
     private BufferedReader in;
@@ -26,11 +27,13 @@ public class ClientHandler extends Thread {
         }
     }
 
-    public void readCommand() throws IOException {
+    public synchronized void readCommand() throws IOException {
         String[] clientCommand = this.in.readLine().split(" ");
 
         switch (clientCommand[0]) {
-            case "Connection:", "Disconnection:" -> userUpdate(User.convertionMessageIntoUser(clientCommand));
+            case "Connection:", "Disconnection:"  -> userUpdate(User.convertionMessageIntoUser(clientCommand));
+
+            case "Change:" -> tabUsersUpdate(clientCommand);
 
             case "Message" -> messageReceived(User.convertionMessageIntoMessage(clientCommand));
 
@@ -38,6 +41,28 @@ public class ClientHandler extends Thread {
     }
 
 }
+
+    public void tabUsersUpdate(String[] clientCommand) {
+        String newUser[] = new String[10];
+        for (int i = 1; i < 11; i++) {
+            String temp = clientCommand[i].split("=")[1];
+            newUser[i-1] = temp.substring(0, temp.length()-1);
+        }
+        controller.getUsers().get(Integer.parseInt(newUser[0])-1).setState(User.State.valueOf(newUser[8]));
+        controller.getUsers().get(Integer.parseInt(newUser[0])-1).setAccess(User.Access.valueOf(newUser[7]));
+
+        if (controller.getCurrentUser() != null) {
+            if (controller.getCurrentUser().getId() == (Integer.parseInt(newUser[0])) && newUser[7].equals("BANNED")) {
+                controller.getCurrentUser().setAccess(User.Access.BANNED);
+                controller.disconnection(false);
+
+            }
+        }
+        if (this.controller.getView2() != null) {
+            this.controller.getView2().repaint();
+        }
+
+    }
 
     public void userUpdate(User user) {
         this.controller.setUser(user);
