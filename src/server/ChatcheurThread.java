813f6df4cf7631a0cbbe7c79354e3675d382f32a
gabriel <gabriel.henaux@edu.ece.fr>
2023-04-25 17:06:03 +0200

    commentaires Server

--- src/server/ChatcheurThread.java
+++ src/server/ChatcheurThread.java
@@ -1,79 +1,102 @@
 package server;
 
-import DAO.LogDao;
-import DAO.MessageDao;
-import DAO.UserDao;
-import controller.ClientController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
 
-import java.io.*;
-import java.net.*;
-import java.util.IllegalFormatCodePointException;
+/**
+ * CHATCHEUR THREAD
+ * --> Instances of these classes are created by the server when a new client connects to the server.
+ */
 
 public class ChatcheurThread implements Runnable {
+
+    /**
+     * ATTRIBUTES
+     */
     private final ChatcheurServer chatcheurServer;
     private final Socket clientSocket;
     private boolean threadRunning = true;
-    private PrintWriter out;
     private BufferedReader in;
     private int numClient = 0;
-    private Thread thread;
-    private LogDao logDao = new LogDao();
-    private MessageDao messageDao = new MessageDao();
-    private UserDao userDao = new UserDao();
 
+    /**
+     * CONSTRUCTOR
+     */
     public ChatcheurThread(Socket s, ChatcheurServer chatcheurServer) {
         this.clientSocket = s;
         this.chatcheurServer = chatcheurServer;
         try {
             this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             this.numClient = chatcheurServer.addClient(out);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //on démarre le thread
-        thread = new Thread(this);
+        Thread thread = new Thread(this);
         thread.start();
     }
 
+    /**
+     * METHODS
+     * <p>
+     * Run the thread.
+     * Read the command sent by the client.
+     * Send the message to the server.
+     * Close the socket.<br>
+     * <p>
+     */
+    @Override
     public void run() {
-        //On indique dans la console qu'un nouveau client est connecte
+
         System.out.println("New client connected, num " + numClient);
 
         try {
             while (threadRunning) {
                 readCommand();
             }
+
         } catch (SocketException e) {
             threadRunning = false;
+
         } catch (IOException e) {
             throw new RuntimeException(e);
+
         } finally {
             System.out.println("Le client no " + this.numClient + " s'est deconnecte");
             this.chatcheurServer.deleteClient(this.numClient);
+
         }
     }
 
+    /**
+     * Read the command sent by the client.
+     */
     public void readCommand() throws IOException {
+
+        //We transform the string into an array of words to be able to sort it
         String[] clientCommand = this.in.readLine().split(" ");
 
         switch (clientCommand[0]) {
-            case "Connection:", "Disconnection:", "Message", "Change:", "Role:" ->
-                    this.chatcheurServer.sendAllMessage(tableauToMessage(clientCommand));
-            case "End" -> {
-                clientSocket.close();
-            }
+            case "Connection:", "Disconnection:", "Message", "Change:", "Role:" -> this.chatcheurServer.sendAllMessage(arrayToMessage(clientCommand));
+            case "End" ->  clientSocket.close();
             default -> System.out.println("default");
         }
     }
 
-    public String tableauToMessage(String[] words) {
-        String string = "";
+    /**
+     * Transform an array of words into a string.
+     */
+    public String arrayToMessage(String[] words) {
+
+        StringBuilder string = new StringBuilder();
         for (String message : words) {
-            string += message + " ";
+            string.append(message).append(" ");
         }
-        return string;
+        return string.toString();
     }
 
 }
